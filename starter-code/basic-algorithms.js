b3f2c2bd30d7735b8b8eb02ac4c3dca4e51dfb2c
Michal Cizevskij <michael.chizhevskiy@hotmail.com>
2017-10-16 18:40:24 +0200

    changed background

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,33 @@
-// Names and Input
 
-console.log(`ff`)
-//Conditionals
+var hacker1 = "Misha"
+var hacker2 = prompt("Whats your Navigators name?")
 
+console.log("The Navigators Name is", hacker2)
 
-// Lorem ipsum generator
+if(hacker1.length > hacker2.length){
+  console.log("The driver has the longest name it has", hacker1.length, "characters")
+}
+else if(hacker2.length > hacker1.length ){
+  console.log("Yo navigator has the longest name it has", hacker2.length, "characters")
+}
+else if(hacker1.length === hacker2.length){
+  console.log("Wow you got equally long names",hacker2.length,"characters")
+}
+  else{
+    console.log("You fucked up")
+
+  }
+var splitAndUppercase = ""
+for(var i = 0; i < hacker1.length ; i++ ){
+
+  splitAndUppercase +=  hacker1[i].toUpperCase() + " "
+
+}
+var printNameInReverse = ""
+console.log(splitAndUppercase)
+
+for (var k = hacker2.length - 1; k >= 0 ; k-- ){
+
+  printNameInReverse += hacker2[k]
+}
+console.log(printNameInReverse)
